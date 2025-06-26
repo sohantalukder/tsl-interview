b57e78fad9a368db5f47775ec535401de2cfb693
sohantalukder <sohan15-1933@diu.edu.bd>
2025-06-26 23:33:13 +0600

    Initializes project structure
    Sets up the basic project structure with core directories

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MainIndex = () => {
+  return (
+    <View>
+      <Text>MainIndex</Text>
+    </View>
+  )
+}
+
+export default MainIndex
