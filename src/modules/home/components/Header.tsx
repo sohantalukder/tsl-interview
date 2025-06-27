16b2525a3e4a211a9d8a155c8a5062408f33b84d
sohantalukder <sohan15-1933@diu.edu.bd>
2025-06-27 20:07:32 +0600

    Add Home screen header

---
+++ src/modules/home/components/Header.tsx
@@ -0,0 +1,37 @@
+import { Pressable, View } from 'react-native';
+import React from 'react';
+import { IconByVariant, Image, Text, TextInput } from '@/shared/components/atoms';
+import rs from '@/shared/utilities/responsiveSize';
+import layout from '@/theme/layout';
+import { useTheme } from '@/theme';
+
+const Header = () => {
+  const { gutters } = useTheme();
+  return (
+    <View style={gutters.gap_16}>
+      <Pressable style={[layout.row, layout.itemsCenter, gutters.gap_10]}>
+        <Image
+          source={{ uri: '' }}
+          style={{ width: rs(40), height: rs(40) }}
+          resizeMode="contain"
+          borderRadius={100}
+        />
+        <View style={gutters.gap_2}>
+          <Text
+            variant="body2"
+            color="secondary"
+          >
+            Welcome back,{' '}
+          </Text>
+          <Text weight="bold">Sohan Talukder 👋</Text>
+        </View>
+      </Pressable>
+      <TextInput
+        placeholder="Search Products ..."
+        leftIcon={<IconByVariant path="search" />}
+      />
+    </View>
+  );
+};
+
+export default Header;
--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import React from 'react';
+import { SafeScreen } from '@/shared/components/templates';
+import Header from './components/Header';
+import { useTheme } from '@/theme';
 const HomeIndex = () => {
+  const { gutters } = useTheme();
   return (
-    <View>
-      <Text>HomeIndex</Text>
-    </View>
-  )
-}
+    <SafeScreen style={gutters.paddingHorizontal_20}>
+      <Header />
+    </SafeScreen>
+  );
+};
 
 export default HomeIndex;
