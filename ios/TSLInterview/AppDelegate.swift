9832542435d4802e7be84024d69e03a93de898f6
sohantalukder <sohan15-1933@diu.edu.bd>
2025-06-28 17:11:10 +0600

    Revert "Adds map view with user location tracking"
    This reverts commit 6ac5e25da5993a0508e6cefbe2583fca5e464e19.

--- ios/TSLInterview/AppDelegate.swift
+++ ios/TSLInterview/AppDelegate.swift
@@ -2,7 +2,6 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
-import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,13 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    // Initialize Google Maps with API key
-    if let path = Bundle.main.path(forResource: "Info", ofType: "plist"),
-       let plist = NSDictionary(contentsOfFile: path),
-       let apiKey = plist["GMSApiKey"] as? String {
-      GMSServices.provideAPIKey(apiKey)
-    }
-    
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
--- src/services/location/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as locationService } from './location.service';
-export { default as useLocation } from './useLocation.hook';
-export type * from '@/types/location';
